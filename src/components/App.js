ba84dbafa514da4e93479d3ea73fd7802b2d8fb4
head-node <sudhanshuoff@gmail.com>
2021-03-10 20:46:25 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date() };
+  }
+  componentDidMount() {
+    // create the interval once component is mounted
+    this.setDate = setInterval(() => {
+      this.setState({ time: new Date() });
+    },1000); // every 1 seconds
+  }
+  componentWillUnmount() {
+    // delete the interval just before component is removed
+    clearInterval(this.setDate);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
